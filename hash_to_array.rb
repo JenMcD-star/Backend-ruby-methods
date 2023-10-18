b62d725ffc39a0db73dd2d62a8ade243ea7e47d4
JenMcD-Star <jenniferannvancura@gmail.com>
2023-10-18 12:08:07 -0500

    hash to array completed

--- hash_to_array.rb
+++ hash_to_array.rb
@@ -1,4 +1,20 @@
-#Write a program that collects 5 keys and 5 values from the user, and stores them in a hash. 
-# Write a function that accepts the hash as a parameter and prints out an array of keys and an array of values.  
-# Call the function within your program so you know it works. 
-#(Question: Can you find online information on Ruby hash methods that will help with this function?)  Call this program hash_to_array.rb.
+def hash_to_array
+#initalize empty hash to store user input
+user_hash = {}
+#loop until the correct number of values/keys are stored
+while user_hash.length < 5
+    puts "Enter key"
+    key = gets.chomp
+    
+    puts "Enter value"
+    value = gets.chomp
+
+    #put each key/value pair into the hash
+    user_hash.store(key, value)
+end
+#print an array of keys and an array of values
+p user_hash.keys, user_hash.values
+end
+
+
+hash_to_array
